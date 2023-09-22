4c851d9d4d85dc939cc1c376937234e21358e098
BalamuruganElaiyaperumal <bala130594@gmail.com>
2023-09-22 16:13:36 +0530

    success

---
+++ src/main/java/com/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class Demo {
+
+	
+	
+	@GetMapping(value="/get")
+	public String get() {
+		return "Welcome Kaai Kamal";
+	}
+}
